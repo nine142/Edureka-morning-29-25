43ba2c028d995cd75e24cdd5bf1c25829d4a7546
nine142 <antonine273@yahoo.com>
2025-03-04 22:09:22 +0000

    two in one

--- sample.py
+++ sample.py
@@ -1,10 +1,11 @@
 a=539
-b=614
+iIb=614
+c=897
 
-<<<<<<< HEAD
-sum = a+b
+ HEAD
+sum = a+b+c
 print "Sum of two numbers is :" sum
-=======
+
 Sum= a+b
-Print "sum of two numbers" is: "sum"
->>>>>>> eff95a5 (My first commit)
+Print "sum of three numbers" is: "sum"
+eff95a5 (My first commit)
